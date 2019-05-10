3d60e279d12d9ff7e94eed81fba2afdc5b4cdefc
mdzoon <mdzoon@hotmail.com>
2019-05-10 01:01:04 +0100

    vue template added

---
+++ wp-content/themes/vuetheme/templates/vueTemplate.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 
+ * Template Name: Vue Map
+ * 
+ * The template for displaying vue map
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+
+			endwhile; // End of the loop.
+			?>
+            <div>Map test<div>
+			<div id="appMap"></div>
+    <!-- built files will be auto injected -->
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
